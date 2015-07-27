7aa144dde7ba1376098fce33d08c49e1ad38aa8a
François Juteau <Lowan@MacBook-Pro-de-Francois.local>
2015-07-27 01:59:47 -0700

    Test de la memoire avec UIbutton

--- SimpleCalculatrice/ViewController.h
+++ SimpleCalculatrice/ViewController.h
@@ -10,6 +10,55 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic) NSString *operand1;
+@property (nonatomic) NSString *operand2;
+
+@property (nonatomic) float result;
+
+@property (weak, nonatomic) UIButton *nextOperation;
+
+@property (weak, nonatomic) IBOutlet UITextField *label;
+
+
+#pragma mark - Inits and Resets
+
+-(id)initWithCoder:(NSCoder *)aDecoder;
+
+#pragma mark - Buttons Properties
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonPlus;
+@property (weak, nonatomic) IBOutlet UIButton *buttonMoins;
+@property (weak, nonatomic) IBOutlet UIButton *buttonMulti;
+@property (weak, nonatomic) IBOutlet UIButton *buttonDiv;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEqual;
+
+
+#pragma mark - Memory properties
+
+@property (strong, atomic) NSMutableArray *memoryArray;
+@property (nonatomic) NSInteger nbCurrentMemory;
+
+@property (weak, nonatomic) IBOutlet UIButton *memory1;
+@property (weak, nonatomic) IBOutlet UIButton *memory2;
+@property (weak, nonatomic) IBOutlet UIButton *memory3;
+@property (weak, nonatomic) IBOutlet UIButton *memory4;
+@property (weak, nonatomic) IBOutlet UIButton *memory5;
+@property (weak, nonatomic) IBOutlet UIButton *memory6;
+
+
+#pragma mark - Buttons Methodes
+
+- (IBAction)pushNumericButton:(UIButton *)sender;
+- (IBAction)pushOperationButton:(UIButton *)sender;
+- (IBAction)pushEqualButton:(UIButton *)sender;
+- (IBAction)pushCancelButton:(UIButton *)sender;
+
+
+#pragma mark - Memory Methode
+
+- (IBAction)AddMemory:(id)sender;
+- (IBAction)memoryAcces:(id)sender;
+
 
 @end
 
--- SimpleCalculatrice/ViewController.m
+++ SimpleCalculatrice/ViewController.m
@@ -14,14 +14,167 @@
 
 @implementation ViewController
 
+#pragma mark - Behaviour
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setNbCurrentMemory:0];
+    
+    NSLog(@"viewDidLoad");
+    
+    [self reset];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark - Inits & Resets
+
+// C'est initWithCoder qui est appelé par le storyboard
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    NSLog(@"init");
+    if (self)
+    {
+        _memoryArray = [[NSMutableArray alloc] initWithObjects:_memory1, _memory2, _memory3, _memory4,_memory5, _memory6, nil];
+    }
+    return self;
+}
+
+-(void)reset
+{
+    [_label setText:@""];
+    [_nextOperation setSelected:NO];
+    
+    [self setNextOperation:nil];
+    [self setOperand1:@""];
+    [self setOperand2:@""];
+    [self setResult:0];
+}
+
+
+#pragma mark - Buttons Methodes
+
+-(IBAction)pushNumericButton:(UIButton *)sender
+{
+    [self pushButtonWithInt:[[NSString alloc] initWithFormat:@"%ld", [sender tag]]];
+}
+
+-(IBAction)pushOperationButton:(UIButton *)sender
+{
+    [self testOperations:sender];
+    
+    [_nextOperation setSelected:YES];
+}
+
+- (IBAction)pushEqualButton:(UIButton *)sender
+{
+    [self testOperations:sender];
+}
+
+- (IBAction)pushCancelButton:(UIButton *)sender
+{
+    if ([_nextOperation isSelected] || _nextOperation == _buttonEqual)
+    {
+        [self reset];
+    }
+    else
+    {
+        // On prend les caractères du text du label jusqu'à sa taille -1
+        [_label setText:[[_label text] substringToIndex:[[_label text] length]-1]];
+    }
+}
+
+
+#pragma mark - Memory Methoes
+
+- (IBAction)AddMemory:(id)sender {
+    
+    long i = _nbCurrentMemory;
+    
+    while ( i > 0 )
+    {
+        [(UIButton *)[_memoryArray objectAtIndex:i] setTitle:[[(UIButton *)[_memoryArray objectAtIndex:i-1] titleLabel] text] forState:UIControlStateNormal];
+        i--;
+    }
+    
+    [(UIButton *)[_memoryArray objectAtIndex:0] setTitle:[_label text] forState:UIControlStateNormal];
+    _nbCurrentMemory++;
+}
+
+- (IBAction)memoryAcces:(UIButton *)sender
+{
+    if ([[[sender titleLabel] text] isEqualToString:@""])
+    {
+        [_label setText:[[sender titleLabel] text]];
+    }
+}
+
+
+#pragma mark - Methodes internes
+
+-(void) pushButtonWithInt:(NSString *)_value
+{
+    if ([_nextOperation isSelected] || _nextOperation == _buttonEqual)
+    {
+        [_label setText:_value];
+        [_nextOperation setSelected:NO];
+    }
+    else
+    {
+        [self addDigit:_value];
+    }
+}
+
+-(void)addDigit:(NSString *)_digit
+{
+    // On assign au text de _label l'ancien text + le paramètre
+    [_label setText:[[_label text] stringByAppendingString:_digit]];
+}
+
+
+-(void)testOperations:(UIButton *)_op
+{
+    _operand2 = [_label text];
+    
+    if (_nextOperation == _buttonPlus)
+    {
+        // Si c'est une multiplication
+        [self setResult:[_operand1 floatValue] + [_operand2 floatValue]];
+    }
+    else if (_nextOperation == _buttonMoins)
+    {
+        // Si c'est une division
+        [self setResult:[_operand1 floatValue] - [_operand2 floatValue]];
+    }
+    else if (_nextOperation == _buttonMulti)
+    {
+        // Si c'est une multiplication
+        [self setResult:[_operand1 floatValue] * [_operand2 floatValue]];
+    }
+    else if (_nextOperation == _buttonDiv)
+    {
+        // Si c'est une division
+        [self setResult:[_operand1 floatValue] / [_operand2 floatValue]];
+    }
+    else
+    {
+        // Si l'objet est nul donc si c'est la première opération
+        [self setResult:[[_label text] floatValue]];
+    }
+    
+    [_nextOperation setSelected:NO];
+    [self setNextOperation:_op];
+    
+    [self setOperand1:[[NSString alloc] initWithFormat:@"%f", [self result]]];
+    [_label setText:[self operand1]];
+}
+
 
 @end
